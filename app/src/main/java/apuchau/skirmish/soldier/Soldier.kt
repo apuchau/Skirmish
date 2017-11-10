4d7d22c1f1b015b7f5c072110fe4050be5b29833
apuchau <alejandro.puchaujara@springernature.com>
2017-11-10 11:00:42 +0000

    Remove unnecessary semicolons

--- app/src/main/java/apuchau/skirmish/soldier/Soldier.kt
+++ app/src/main/java/apuchau/skirmish/soldier/Soldier.kt
@@ -7,12 +7,12 @@ class Soldier(val soldierId: SoldierId) {
     }
 
     override fun hashCode(): Int {
-        return soldierId.hashCode();
+        return soldierId.hashCode()
     }
 
     override fun equals(other: Any?): Boolean {
         return (other != null)
            && (other is Soldier)
-           && (soldierId == other.soldierId);
+           && (soldierId == other.soldierId)
     }
 }
