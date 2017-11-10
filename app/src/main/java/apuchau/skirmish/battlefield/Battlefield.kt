bf2986772dbd89ed50d3c59c478d4fbf372c99ae
apuchau <alejandro.puchaujara@springernature.com>
2017-11-10 11:01:55 +0000

    Add toString method

--- app/src/main/java/apuchau/skirmish/battlefield/Battlefield.kt
+++ app/src/main/java/apuchau/skirmish/battlefield/Battlefield.kt
@@ -2,6 +2,10 @@ package apuchau.skirmish.battlefield
 
 class Battlefield(val boundaries : BattlefieldBoundaries) {
 
+    override fun toString(): String {
+        return "Battlefield. $boundaries"
+    }
+
     override fun hashCode(): Int {
         return boundaries.hashCode()
     }
