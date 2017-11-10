d6144343a63f30718a74ece0634f4d51432982d9
apuchau <alejandro.puchaujara@springernature.com>
2017-11-10 11:00:16 +0000

    Add toString method

--- app/src/main/java/apuchau/skirmish/soldier/Soldier.kt
+++ app/src/main/java/apuchau/skirmish/soldier/Soldier.kt
@@ -2,6 +2,10 @@ package apuchau.skirmish.soldier
 
 class Soldier(val soldierId: SoldierId) {
 
+    override fun toString(): String {
+        return "Soldier $soldierId"
+    }
+
     override fun hashCode(): Int {
         return soldierId.hashCode();
     }
